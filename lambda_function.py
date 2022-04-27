612325c80a0354e6b118d63d3241aa335a737ace
sirisagi <86584653+sirisagi@users.noreply.github.com>
2022-04-27 19:34:18 +0530

    updated the code after removing call function

--- lambda_function.py
+++ lambda_function.py
@@ -9,14 +9,16 @@ def upload_files(event, context):
     file_prefix = os.environ.get('FILE_PREFIX')
     baseline_file = os.environ.get('BASELINE_FILE')
     bucket, filename = bucket_name, file_prefix
+    print(bucket)
+    print(filename)
     s3 = boto3.resource('s3')
     obj = s3.Object(bucket, filename)
     with BytesIO(obj.get()['Body'].read()) as bio:
         df = pd.read_json(bio, lines=True)
         splits = list(df.groupby("order_status"))
+        print(len(splits))
         for split in splits:
             s3_url = f'{baseline_file}/{split[0]}.parquet.gzip'
             split[1].to_parquet(s3_url, compression='gzip')
 
 
-upload_files(None, None)
